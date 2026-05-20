510d02b5a95306da14d40323b56df2d47e43af63
Arun Kumar Mudam <arunkumar.mudam@magnaquest.com>
2026-05-20 12:15:33 +0530

    Initial project commit

---
+++ IncognitoSimulator/src/main/java/com/incognito/soap/SoapSimulatorResponses.java
@@ -0,0 +1,187 @@
+package com.incognito.soap;
+
+
+public class SoapSimulatorResponses {
+
+    public static String loginResponse(String token) {
+    	return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+    		       "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+    		       "<soapenv:Body>" +
+    		       "<n:loginResponse xmlns:n=\"http://bcc.incognito.com/IncEntitySecurity\" xmlns:n0=\"http://www.incognito.com/IncTypes\">" +
+    		       "<n:authorizationToken>" +
+    		       "<n0:token>" + token + "</n0:token>" +
+    		       "</n:authorizationToken>" +
+    		       "</n:loginResponse>" +
+    		       "</soapenv:Body>" +
+    		       "</soapenv:Envelope>";
+    }
+
+    public static String createResponse(String id, String entityId, String name, String mac) {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+               "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+               "<soapenv:Body>\n" +
+               "<n:createRecordsResponse xmlns:n=\"http://bcc.incognito.com/MPService\" xmlns:n0=\"http://www.incognito.com/IncTypes\">" +
+               "<n:cableModems>" +
+               "<n:values>" +
+               "<n0:name>" + name + "</n0:name>" +
+               "<n0:description>Internet Subscriber</n0:description>" +
+               "<n0:creationTime>2025-04-16T09:39:12.000Z</n0:creationTime>" +
+               "<n0:lastModified>2025-04-16T09:39:12.000Z</n0:lastModified>" +
+               "<n0:lastModifiedBy>magnatest</n0:lastModifiedBy>" +
+               "<n0:id>" + id + "</n0:id>" +
+               "<n0:entityId>" + entityId + "</n0:entityId>" +
+               "<n:macAddress>" + mac + "</n:macAddress>" +
+               "<n:fqdn/>" +
+               "<n:maxCPEs>0</n:maxCPEs>" +
+               "<n:clientClasses>30M</n:clientClasses>" +
+               "<n:clientClasses>MAX-CPEs-1</n:clientClasses>" +
+               "<n:staticAddresses/>" +
+               "</n:values>" +
+               "</n:cableModems>" +
+               "</n:createRecordsResponse>" +
+               "</soapenv:Body>" +
+               "</soapenv:Envelope>";
+    }
+
+    public static String permissionDenied() {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+               "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+               "<soapenv:Body>" +
+               "<n:searchRecordsResponse xmlns:n=\"http://bcc.incognito.com/MPService\" xmlns:n0=\"http://www.incognito.com/IncTypes\">" +
+               "<n:errorCode>" +
+               "<n0:hasError>true</n0:hasError>" +
+               "<n0:status>STATUS_ERROR</n0:status>" +
+               "<n0:problemCode>13</n0:problemCode>" +
+               "<n0:problemMessage>Permission denied</n0:problemMessage>" +
+               "</n:errorCode>" +
+               "<n:nextOffset>0</n:nextOffset>" +
+               "<n:totalRecordCount>0</n:totalRecordCount>" +
+               "</n:searchRecordsResponse>" +
+               "</soapenv:Body>" +
+               "</soapenv:Envelope>";
+    }
+    
+    public static String permissionDeniedInvalidMAC() {
+        return "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                "<soapenv:Body>" +
+                "<n:createRecordsResponse xmlns:n=\"http://bcc.incognito.com/MPService\" xmlns:n0=\"http://www.incognito.com/IncTypes\">" +
+                "<n:errors>" +
+                "<n0:values>" +
+                "<n0:errorCode>" +
+                "<n0:hasError>true</n0:hasError>" +
+                "<n0:status>STATUS_ERROR</n0:status>" +
+                "<n0:problemCode>22</n0:problemCode>" +
+                "<n0:problemMessage>Invalid argument</n0:problemMessage>" +
+                "</n0:errorCode>" +
+                "<n0:id>" +
+                "<n0:id></n0:id>" +
+                "<n0:dbTable>DOCSIS_CABLEMODEM</n0:dbTable>" +
+                "</n0:id>" +
+                "</n0:values>" +
+                "</n:errors>" +
+                "</n:createRecordsResponse>" +
+                "</soapenv:Body>" +
+                "</soapenv:Envelope>";
+    }
+    
+    public static String searchRecordsResponse(boolean success, int totalRecords) {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+               "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:n=\"http://bcc.incognito.com/MPService\" xmlns:n0=\"http://www.incognito.com/IncTypes\">" +
+               "<soapenv:Body>" +
+               "<n:searchRecordsResponse>" +
+               "<n:errorCode>" +
+               "<n0:hasError>" + ("false") + "</n0:hasError>" +
+               "<n0:status>" + ("STATUS_SUCCESS") + "</n0:status>" +
+               "</n:errorCode>" +
+               "<n:nextOffset>0</n:nextOffset>" +
+               "<n:totalRecordCount>" + totalRecords + "</n:totalRecordCount>" +
+               "</n:searchRecordsResponse>" +
+               "</soapenv:Body>" +
+               "</soapenv:Envelope>";
+    }
+    
+    public static String incSearchRecordsResponse() {
+        return "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                "<soapenv:Body>" +
+                "<n:searchRecordsResponse xmlns:n=\"http://bcc.incognito.com/MPService\" xmlns:n0=\"http://www.incognito.com/IncTypes\">" +
+                "<n:errorCode>" +
+                "<n0:hasError>false</n0:hasError>" +
+                "<n0:status>STATUS_SUCCESS</n0:status>" +
+                "</n:errorCode>" +
+                "<n:cableModems>" +
+                "<n:values>" +
+                "<n0:name>ARUN</n0:name>" +
+                "<n0:description>Internet Subscriber</n0:description>" +
+                "<n0:creationTime>2025-05-08T07:10:58.000Z</n0:creationTime>" +
+                "<n0:lastModified>2025-05-08T07:10:58.000Z</n0:lastModified>" +
+                "<n0:lastModifiedBy>QCTester</n0:lastModifiedBy>" +
+                "<n0:id>AAAOKEYAAAOKAYAAAOKAYAAAOKAYAAA</n0:id>" +
+                "<n0:entityId>AAAOKEYAAAOKAYAAAOKAYAAAOKAYAAA</n0:entityId>" +
+                "<n:macAddress>001DCE39C147</n:macAddress>" +
+                "<n:fqdn></n:fqdn>" +
+                "<n:maxCPEs>0</n:maxCPEs>" +
+                "<n:clientClasses>30M</n:clientClasses>" +
+                "<n:clientClasses>MAX-CPEs-1</n:clientClasses>" +
+                "<n:staticAddresses></n:staticAddresses>" +
+                "</n:values>" +
+                "</n:cableModems>" +
+                "<n:nextOffset>0</n:nextOffset>" +
+                "<n:totalRecordCount>27144</n:totalRecordCount>" +
+                "</n:searchRecordsResponse>" +
+                "</soapenv:Body>" +
+                "</soapenv:Envelope>";
+    }
+    
+    public static String mpcreateResponse(String name, String id, String entityId, String notes, String accountId) {
+        String timestamp = java.time.ZonedDateTime.now().toString();
+
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                + "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+                + "<soapenv:Body>"
+                + "<n:createRecordsResponse xmlns:n=\"http://bcc.incognito.com/IncEntitySecurity\" xmlns:n0=\"http://www.incognito.com/IncTypes\">"
+                + "<n:entities>"
+                + "<n:values>"
+                + "<n0:name>" + name + "</n0:name>"
+                + "<n0:description>Internet Subscriber</n0:description>"
+                + "<n0:creationTime>" + timestamp + "</n0:creationTime>"
+                + "<n0:lastModified>" + timestamp + "</n0:lastModified>"
+                + "<n0:lastModifiedBy>magnatest</n0:lastModifiedBy>"
+                + "<n0:id>" + id + "</n0:id>"
+                + "<n0:entityId>" + entityId + "</n0:entityId>"
+                + "<n:accountId>" + accountId + "</n:accountId>"
+                + "<n:address>"
+                + "<n0:city/>"
+                + "<n0:state_province/>"
+                + "<n0:zipcode_postalcode/>"
+                + "<n0:country/>"
+                + "</n:address>"
+                + "<n:entityTypeId>3B8634E61BE011DFA78D001A64A3084E</n:entityTypeId>"
+                + "<n:notes>" + notes + "</n:notes>"
+                + "</n:values>"
+                + "</n:entities>"
+                + "</n:createRecordsResponse>"
+                + "</soapenv:Body>"
+                + "</soapenv:Envelope>";
+    }
+    
+    public static String mpUpdateRecordResponse(String request) {
+    	return request;
+    }
+    public static String incDeleteRecordResponse() {
+    	return "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+    			+ "<soapenv:Body>"
+    			+ "<n:deleteRecordsResponse xmlns:n=\"http://bcc.incognito.com/IncEntitySecurity\"/>"
+    			+ "</soapenv:Body>"
+    			+ "</soapenv:Envelope>";
+    }
+    public static String mpDeleteRecordResponse() {
+    	return "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+    			+ "<soapenv:Body>"
+    			+ "<n:deleteRecordsResponse xmlns:n=\"http://bcc.incognito.com/MPService\"/>"
+    			+ "</soapenv:Body>"
+    			+ "</soapenv:Envelope>";
+    }
+
+}
+
+
---
+++ IncognitoSimulator/src/main/java/com/incognito/soap/SoapSimulatorServlet.java
@@ -0,0 +1,205 @@
+package com.incognito.soap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author arunkumar.mudam
+ *
+ */
+
+@WebServlet("/soap")
+public class SoapSimulatorServlet extends HttpServlet {
+
+    private long tokenExpiryMillis = 5 * 60 * 1000; // default: 5 minutes
+    private long firstConnectionTime = 0;
+
+    private Map<String, Long> recentTokens = new LinkedHashMap<>();
+    private static final int MAX_TOKENS = 5;
+
+    private Set<String> validMacAddresses = new HashSet<>();
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try (InputStream input = getServletContext().getResourceAsStream("/WEB-INF/simulator.properties")) {
+            Properties props = new Properties();
+            if (input != null) {
+                props.load(input);
+
+                String macList = props.getProperty("valid.mac.addresses");
+                if (macList != null && !macList.trim().isEmpty()) {
+                    validMacAddresses.addAll(Arrays.asList(macList.trim().split("\\s*,\\s*")));
+                }
+
+                String expiry = props.getProperty("token.expiry.millis");
+                if (expiry != null && !expiry.trim().isEmpty()) {
+                    tokenExpiryMillis = Long.parseLong(expiry.trim());
+                }
+            }
+        } catch (IOException e) {
+            throw new ServletException("Unable to load config.properties", e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = null;
+        try {
+            pw = response.getWriter();
+            response.setContentType("text/html");
+            pw.println("<html><head>");
+            pw.println("<link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap' rel='stylesheet'>");
+            pw.println("</head><body style='background-color: #f4f4f9;'>");
+            pw.println("<center>");
+            pw.println("<p style='font-size: 24px; color: #3498db; font-family: Poppins, Arial, sans-serif; font-weight: bold; margin-top: 50px;'>");
+            pw.println("Incognito Simulator</p>");
+            pw.println("<hr style='width: 50%; border: 2px solid #3498db;'>");
+            pw.println("<p style='font-size: 18px; color: #2ecc71; font-family: Poppins, Arial, sans-serif; font-weight: normal;'>");
+            pw.println("Running on port: <span style='color: #e74c3c;'>" + request.getLocalPort() + "</span></p>");
+            pw.println("</center>");
+            pw.println("</body></html>");
+
+        } catch (Exception e) {
+            if (pw != null) pw.println("<errores>INTERNAL ERROR</errores>");
+        } finally {
+            if (pw != null) pw.close();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String xmlRequest = readBody(request);
+        String soapResponse;
+        //for login request generates new token and stores in recenttokens list until expiry
+        if (xmlRequest.contains("<inc:login>")) {
+
+            if (firstConnectionTime == 0)
+                firstConnectionTime = System.currentTimeMillis() / 1000;
+
+            if ((System.currentTimeMillis() / 1000) - firstConnectionTime > tokenExpiryMillis) {
+                firstConnectionTime = System.currentTimeMillis() / 1000;
+                recentTokens.clear();
+            }
+
+            String newToken = UUID.randomUUID().toString().replace("-", "");
+            recentTokens.put(newToken, System.currentTimeMillis());
+
+            if (recentTokens.size() > MAX_TOKENS) {
+                Iterator<String> it = recentTokens.keySet().iterator();
+                if (it.hasNext()) {
+                    it.next();
+                    it.remove();
+                }
+            }
+
+            soapResponse = SoapSimulatorResponses.loginResponse(newToken);
+
+        } else if (xmlRequest.contains("<mps:createRecords>")) {
+            String token = extractTagValue(xmlRequest, "inc:token");
+            String entityId = extractTagValue(xmlRequest, "inc:entityId");
+            String name = extractTagValue(xmlRequest, "inc:name");
+            String mac = extractTagValue(xmlRequest, "mps:macAddress");
+
+            if (isTokenValid(token)) {
+            	if (validMacAddresses.contains(mac)) {
+                String id = UUID.randomUUID().toString().replace("-", "").substring(0, 32).toUpperCase();
+                soapResponse = SoapSimulatorResponses.createResponse(id, entityId, name, mac);
+            	}else {
+            		soapResponse = SoapSimulatorResponses.permissionDeniedInvalidMAC();
+            	}
+            } else {
+                soapResponse = SoapSimulatorResponses.permissionDenied();
+            }
+
+        } else if (xmlRequest.contains("<mps:searchRecords>")) {
+            String token = extractTagValue(xmlRequest, "inc:token");
+            String criteria = extractTagValue(xmlRequest, "mps:criteria");
+
+            if (isTokenValid(token)) {
+                if (criteria != null && criteria.startsWith("MACADDRESS=")) {
+                    String mac = criteria.split("=")[1];
+                    if (validMacAddresses.contains(mac)) {
+                        soapResponse = SoapSimulatorResponses.searchRecordsResponse(false, 0);
+                    } else {
+                        soapResponse = SoapSimulatorResponses.permissionDeniedInvalidMAC();
+                    }
+                } else {
+                    soapResponse = SoapSimulatorResponses.permissionDeniedInvalidMAC();
+                }
+            } else {
+                soapResponse = SoapSimulatorResponses.permissionDenied();
+            }
+
+        } else if (xmlRequest.contains("<inc:createRecords>")) {
+            String token = extractTagValue(xmlRequest, "inc1:token");
+            String accountId = extractTagValue(xmlRequest, "inc:accountId");
+            String name = extractTagValue(xmlRequest, "inc1:name");
+            String notes = extractTagValue(xmlRequest, "inc:notes");
+//            String macAddress=extractTagValue(xmlRequest, "mps:macAddress");
+
+            if (isTokenValid(token)) {
+//            	if (validMacAddresses.contains(macAddress)) {
+            		String id = UUID.randomUUID().toString().replace("-", "").substring(0, 32).toUpperCase();
+                    String entityId = UUID.randomUUID().toString().replace("-", "").substring(0, 32).toUpperCase();
+                    soapResponse = SoapSimulatorResponses.mpcreateResponse(name, id, entityId, notes, accountId);
+//            	}else {
+//            		soapResponse = SoapSimulatorResponses.permissionDeniedInvalidMAC();
+//            	}
+                
+            } else {
+                soapResponse = SoapSimulatorResponses.permissionDenied();
+            }
+
+        } else if (xmlRequest.contains("<mps:updateRecords>")) {
+            soapResponse = SoapSimulatorResponses.mpUpdateRecordResponse(xmlRequest);
+
+        } else if (xmlRequest.contains("<inc:deleteRecords>")) {
+            soapResponse = SoapSimulatorResponses.incDeleteRecordResponse();
+
+        } else if (xmlRequest.contains("<mps:deleteRecords>")) {
+            soapResponse = SoapSimulatorResponses.mpDeleteRecordResponse();
+
+        } else if (xmlRequest.contains("<inc:searchRecords>")) {
+        	soapResponse = SoapSimulatorResponses.incSearchRecordsResponse();
+        }else {
+            soapResponse = SoapSimulatorResponses.permissionDenied();
+        }
+
+        response.setContentType("text/xml;charset=UTF-8");
+        response.getWriter().write(soapResponse);
+    }
+
+    private boolean isTokenValid(String token) {
+        Long createdTime = recentTokens.get(token);
+        if (createdTime == null) return false;
+
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - createdTime > tokenExpiryMillis) {
+            recentTokens.remove(token); // Expired token
+            return false;
+        }
+        return true;
+    }
+
+    private String readBody(HttpServletRequest request) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null)
+            sb.append(line);
+        return sb.toString();
+    }
+
+    private String extractTagValue(String xml, String tag) {
+        int start = xml.indexOf("<" + tag + ">");
+        int end = xml.indexOf("</" + tag + ">");
+        if (start == -1 || end == -1)
+            return "";
+        return xml.substring(start + tag.length() + 2, end).trim();
+    }
+}
